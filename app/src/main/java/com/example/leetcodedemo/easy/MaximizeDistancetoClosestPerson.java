c6cf782700f1fa93cab9b013278d8c5845cc8987
wcxdhr <wangchunxuedhr@163.com>
2019-09-03 17:42:45 +0800

    2019.9.3

--- app/src/main/java/com/example/leetcodedemo/easy/MaximizeDistancetoClosestPerson.java
+++ app/src/main/java/com/example/leetcodedemo/easy/MaximizeDistancetoClosestPerson.java
@@ -5,9 +5,35 @@ package com.example.leetcodedemo.easy;
  */
 
 public class MaximizeDistancetoClosestPerson {
-    public int maxDistToClosest(int[] seats) {
-        int ans  = 1;
 
+    /**
+     * 更优解法：找最大0距离，一遍循环
+     * @param seats
+     * @return
+     */
+    public int maxDistToClosest(int[] seats) {
+        int index = -1;
+        int ans = 1;
+        int[] minDist = new int[seats.length];
+        for(int i = 0; i < seats.length; i++) {
+            if(seats[i] == 1) {
+                index = i;
+            }
+            else if (index != -1)
+                minDist[i] = i - index;
+
+        }
+        index = -1;
+        for(int i = seats.length - 1; i >= 0; i--) {
+            if(seats[i] == 1)
+                index = i;
+            else {
+                if (index != -1){
+                    minDist[i] = minDist[i] != 0 ? Math.min(minDist[i], index - i) : index - i;
+                }
+                ans = Math.max(minDist[i], ans);
+            }
+        }
         return ans;
     }
 }
