3bd2ac2cda0bc422c6c0302a053b9f3fc0a08531
wcxdhr <wangchunxuedhr@bupt.edu.cn>
2020-01-21 21:12:30 +0800

    2020.1.21

--- app/src/main/java/com/example/leetcodedemo/string/medium/LongestPalindromicSubstring.java
+++ app/src/main/java/com/example/leetcodedemo/string/medium/LongestPalindromicSubstring.java
@@ -21,8 +21,84 @@ package com.example.leetcodedemo.string.medium;
  * Created by Wcxdhr on 2019/10/11.
  */
 public class LongestPalindromicSubstring {
-    public String longestPalindrome(String s) {
 
-        return "";
+    public static void main(String[] args) {
+        new LongestPalindromicSubstring().longestPalindrome("yxyslls");
+    }
+
+    /**
+     * 思路：参考中心扩展法，从字符串中间往两边扩展，找最大的回文，感觉判断条件很复杂，代码也很乱。
+     * 题解优化：马车算法
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+        String ans = "";
+        int medium = s.length() / 2;
+        int i = 0;
+        while (medium >= i) {
+            if (ans.length() > (medium - i + 1) * 2)
+                break;
+            int len = isPalindromicHelper(medium + i, medium + i, s);
+            if (len > ans.length())
+                ans = s.substring(medium + i - len / 2, medium + i + len / 2 + 1);
+            len = isPalindromicHelper(medium + i, medium + i + 1, s);
+            if (len > ans.length())
+                ans = s.substring(medium + i - len / 2 + 1, medium + i + len / 2 + 1);
+            len = isPalindromicHelper(medium - i, medium - i, s);
+            if (len > ans.length())
+                ans = s.substring(medium - i - len / 2, medium - i + len / 2 + 1);
+            len = isPalindromicHelper(medium - i, medium - i + 1, s);
+            if (len > ans.length())
+                ans = s.substring(medium - i - len / 2 + 1, medium - i + len / 2 + 1);
+            i++;
+        }
+        return ans;
+    }
+
+    private int isPalindromicHelper(int start, int end, String s) {
+        while (start >= 0 && end < s.length()) {
+            if (s.charAt(start) != s.charAt(end))
+                break;
+            start--;
+            end++;
+        }
+        return end - start - 1;
+    }
+
+
+    /**
+     * 题解：动态规划，二维动态规划，思考：回文的判断，怎样可以得到这一段是回文呢？
+     * 取决于当前的ij是否相等，及i和j之间是否是回文
+     * https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zhong-xin-kuo-san-dong-tai-gui-hua-by-liweiwei1419/
+     * @param s
+     * @return
+     */
+    public String longestPalindrome1(String s){
+        if (s.length() < 2)
+            return s;
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++)
+            dp[i][i] = true;
+        int maxLen = 1;
+        int start = 0;
+        for (int j = 0; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i >= 3)
+                        dp[i][j] = dp[i + 1][j - 1];
+                    else
+                        dp[i][j] = true;
+                }
+                else
+                    dp[i][j] = false;
+                if (dp[i][j] && j - i + 1 > maxLen){
+                    maxLen = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+        return s.substring(start, start + maxLen + 1);
     }
 }
